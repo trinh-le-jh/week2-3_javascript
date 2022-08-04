5784d3c8573fa07317dcd0f26cdfad3f522970bd
Trinh Le <110367270+trinh-le-jh@users.noreply.github.com>
2022-08-04 13:41:30 +0700

    optimize exercise2

--- Array and Object/exercise2.js
+++ Array and Object/exercise2.js
@@ -2,15 +2,23 @@ const arr = [1,2,4,4,4,5,6,7,1,2,4,5,7,2,2,1]
 
 const countDuplicate = arr.reduce((acc, cur) => {
     const newTotalDuplicate = (acc.counterList[cur] || 0) + 1
-    acc.counterList[cur]= newTotalDuplicate
+    acc.counterList[cur] = newTotalDuplicate
+    
+    if (newTotalDuplicate > acc.maximumDuplicate) {
+        acc.maximumDuplicate = newTotalDuplicate
+        acc.mostDuplicate.clear()
+        acc.mostDuplicate.add(cur)
+    }
+    
+    if (newTotalDuplicate === acc.maximumDuplicate) {
+        acc.mostDuplicate.add(cur)
+    }
     
     return {
         counterList: acc.counterList,
-        max: (newTotalDuplicate > acc.max ? newTotalDuplicate : acc.max)
+        maximumDuplicate: acc.maximumDuplicate,
+        mostDuplicate: acc.mostDuplicate
     }
-}, {counterList: {}, max: 0})
+}, {counterList: {}, maximumDuplicate: 0, mostDuplicate: new Set()})
 
-const listDuplicate = Object.keys(countDuplicate.counterList).filter((i) =>
-  countDuplicate.counterList[i] == countDuplicate.max)
-
-console.log('Most common item: ' + listDuplicate.join(', '))
+console.log(countDuplicate)
