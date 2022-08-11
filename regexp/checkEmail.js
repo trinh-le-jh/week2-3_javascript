502af8a64dea4115d1e9430fe6361760d620881c
Trinh Le <110367270+trinh-le-jh@users.noreply.github.com>
2022-08-11 13:16:10 +0700

    refactor regexp exercise

---
+++ regexp/checkEmail.js
@@ -0,0 +1,12 @@
+const emailRegexp = new RegExp(
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+);
+
+const validateEmail = (email) => {
+  return emailRegexp.test(email);
+};
+
+console.log(validateEmail('exe@example'));
+console.log(validateEmail('tji.jhjnnn@example.com'));
+console.log(validateEmail('kian_mintzlaff70@yahoo.com'));
+console.log(validateEmail('e9b23900dd30e10aaa@example.com'));
---
+++ regexp/checkPassword.js
@@ -0,0 +1,25 @@
+const passwordRegexp = new RegExp(
+  /^((?=.*[a-z])(?=.*[A-Z]))(?=.*\d)(?=.*[`~!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])[A-Za-z\d`~!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]{8,}$/
+)
+
+const validatePassword = (email) => {
+  return passwordRegexp.test(email);
+};
+
+// Contain all
+console.log(validatePassword('e93W@b239'));
+
+// No special character and upper case
+console.log(validatePassword('e9b23989'));
+
+// No number
+console.log(validatePassword('kian_miUw'));
+
+// No upper case
+console.log(validatePassword('tji.jh2r'));
+
+// No special character
+console.log(validatePassword('exRom2ue'));
+
+// Not long enough
+console.log(validatePassword('aU1#e'));
